29d13d9e12ffeb469969fc787b705959de90d2b9
Caio Tracera <ctrcra@gmail.com>
2023-10-07 00:01:38 -0300

    refactor: delete GET /hello

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,8 @@
 import fastify from 'fastify';
-import { knex } from '@/database';
 
 const DEFAULT_PORT = 3333;
 
 const app = fastify();
-
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
-
-  return tables;
-});
 
 app
   .listen({
