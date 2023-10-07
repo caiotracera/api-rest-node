41c84257058fc25608ec0e28251fe190485ab38c
Caio Tracera <ctrcra@gmail.com>
2023-10-07 12:17:23 -0300

    test: add db for testing

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,14 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { z } from 'zod';
+import * as process from 'process';
+
+if (process.env.NODE_ENV === 'test') {
+  config({
+    path: '.env.test.local',
+  });
+} else {
+  config();
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -1,5 +1,6 @@
-import { it, describe, beforeAll, afterAll, expect } from 'vitest';
+import { it, describe, beforeAll, afterAll, expect, beforeEach } from 'vitest';
 import request from 'supertest';
+import { execSync } from 'node:child_process';
 
 import { app } from '@/app';
 
@@ -10,6 +11,11 @@ describe('Transactions', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all');
+    execSync('npm run knex migrate:latest');
   });
 
   it('should be able to create a new transaction', async () => {
