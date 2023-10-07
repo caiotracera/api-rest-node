2baa3748c32fb7dc584c271a72f8a7dc69962a2b
Caio Tracera <ctrcra@gmail.com>
2023-10-07 12:29:05 -0300

    test: add test for summary endpoint

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -56,4 +56,70 @@ describe('Transactions', () => {
       }),
     );
   });
+
+  it('should be able to get a specific transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Random Transaction',
+        amount: 200,
+        type: 'income',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id;
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(getTransactionResponse.body).toEqual(
+      expect.objectContaining({
+        transaction: expect.objectContaining({
+          title: 'Random Transaction',
+          amount: 200,
+        }),
+      }),
+    );
+  });
+
+  it('should be able to get the summary of the transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Random Transaction',
+        amount: 200,
+        type: 'income',
+      });
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Random Transaction #2',
+        amount: 100,
+        type: 'outcome',
+      });
+
+    const summaryTransactionsResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies);
+
+    expect(summaryTransactionsResponse.body).toEqual(
+      expect.objectContaining({
+        summary: expect.objectContaining({
+          amount: 100,
+        }),
+      }),
+    );
+  });
 });
