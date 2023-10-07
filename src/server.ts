d6e03f340e595d3afd5e404ad4bfd684f1978869
Caio Tracera <ctrcra@gmail.com>
2023-10-06 23:06:16 -0300

    chore: create migration to create transactions

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import fastify from 'fastify';
+import { knex } from '@/database';
 
 const DEFAULT_PORT = 3333;
 
 const app = fastify();
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*');
+
+  return tables;
+});
 
 app
   .listen({
     port: DEFAULT_PORT,
   })
   .then(() => {
-    console.log(`?? Server is running on port ${DEFAULT_PORT}`);
+    console.log(`🚀 Server is running on port ${DEFAULT_PORT}`);
   });
