0c963b3c2acd9319da3dabb3deb92f36b859b33a
Caio Tracera <ctrcra@gmail.com>
2023-10-07 00:09:03 -0300

    feat: add .env support

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,12 @@
 import { knex as setupKnex, Knex } from 'knex';
+import 'dotenv/config';
+
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL not set');
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify';
+import { knex } from '@/database';
 
 const DEFAULT_PORT = 3333;
 
 const app = fastify();
+
+app.get('/', async () => {
+  const transaction = await knex.select('*').from('transactions');
+
+  return transaction;
+});
 
 app
   .listen({
